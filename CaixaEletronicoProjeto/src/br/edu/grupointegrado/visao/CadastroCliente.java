910e356ee87cff849184d4a17d57e6b07062ca00
Jamerson chiminacio <jamerson.cp@gmail.com>
2016-04-25 20:35:12 -0300

    mudança na ordem das telas

--- CaixaEletronicoProjeto/src/br/edu/grupointegrado/visao/CadastroCliente.java
+++ CaixaEletronicoProjeto/src/br/edu/grupointegrado/visao/CadastroCliente.java
@@ -30,12 +30,6 @@ public class CadastroCliente extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
         jTabbedPane1 = new javax.swing.JTabbedPane();
-        jPConsulta = new javax.swing.JPanel();
-        jBConsulta = new javax.swing.JButton();
-        jCBConsulta = new javax.swing.JComboBox<>();
-        jTFConsulta = new javax.swing.JTextField();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
         jPCadastro = new javax.swing.JPanel();
         jTFCodigo = new javax.swing.JTextField();
         jTFNome = new javax.swing.JTextField();
@@ -49,57 +43,18 @@ public class CadastroCliente extends javax.swing.JFrame {
         jBCancelar = new javax.swing.JButton();
         jTFAgencia = new javax.swing.JTextField();
         jTFConta = new javax.swing.JTextField();
+        jPConsulta = new javax.swing.JPanel();
+        jBConsulta = new javax.swing.JButton();
+        jCBConsulta = new javax.swing.JComboBox<String>();
+        jTFConsulta = new javax.swing.JTextField();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
 
         jTextArea1.setColumns(20);
         jTextArea1.setRows(5);
         jScrollPane1.setViewportView(jTextArea1);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jBConsulta.setText("Consultar");
-
-        jCBConsulta.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Código", "Nome", "CPF" }));
-
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-
-            },
-            new String [] {
-                "Código", "Nome", "CPF", "Conta", "Agência"
-            }
-        ));
-        jScrollPane2.setViewportView(jTable1);
-
-        javax.swing.GroupLayout jPConsultaLayout = new javax.swing.GroupLayout(jPConsulta);
-        jPConsulta.setLayout(jPConsultaLayout);
-        jPConsultaLayout.setHorizontalGroup(
-            jPConsultaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPConsultaLayout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(jPConsultaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jScrollPane2)
-                    .addGroup(jPConsultaLayout.createSequentialGroup()
-                        .addComponent(jCBConsulta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 30, Short.MAX_VALUE)
-                        .addComponent(jTFConsulta, javax.swing.GroupLayout.PREFERRED_SIZE, 369, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(jBConsulta)))
-                .addGap(30, 30, 30))
-        );
-        jPConsultaLayout.setVerticalGroup(
-            jPConsultaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPConsultaLayout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(jPConsultaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jBConsulta)
-                    .addComponent(jCBConsulta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTFConsulta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 314, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-
-        jTabbedPane1.addTab("Consulta", jPConsulta);
 
         jTFCodigo.setBorder(javax.swing.BorderFactory.createTitledBorder("Código"));
 
@@ -215,6 +170,51 @@ public class CadastroCliente extends javax.swing.JFrame {
         );
 
         jTabbedPane1.addTab("Cadastro", jPCadastro);
+
+        jBConsulta.setText("Consultar");
+
+        jCBConsulta.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Código", "Nome", "CPF" }));
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Código", "Nome", "CPF", "Conta", "Agência"
+            }
+        ));
+        jScrollPane2.setViewportView(jTable1);
+
+        javax.swing.GroupLayout jPConsultaLayout = new javax.swing.GroupLayout(jPConsulta);
+        jPConsulta.setLayout(jPConsultaLayout);
+        jPConsultaLayout.setHorizontalGroup(
+            jPConsultaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPConsultaLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPConsultaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jScrollPane2)
+                    .addGroup(jPConsultaLayout.createSequentialGroup()
+                        .addComponent(jCBConsulta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 30, Short.MAX_VALUE)
+                        .addComponent(jTFConsulta, javax.swing.GroupLayout.PREFERRED_SIZE, 369, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jBConsulta)))
+                .addGap(30, 30, 30))
+        );
+        jPConsultaLayout.setVerticalGroup(
+            jPConsultaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPConsultaLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPConsultaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jBConsulta)
+                    .addComponent(jCBConsulta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTFConsulta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 314, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        jTabbedPane1.addTab("Consulta", jPConsulta);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
