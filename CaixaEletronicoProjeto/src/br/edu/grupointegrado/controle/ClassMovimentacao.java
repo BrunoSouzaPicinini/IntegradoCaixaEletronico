ae1b4a939ead2976fb18a42b61ffb9303c3f15c5
Bruno Souza Picinini <bruno_souza_picinini@hotmail.com>
2016-05-26 18:30:17 -0300

    Funcionalidades ClassMovimentação

--- CaixaEletronicoProjeto/src/br/edu/grupointegrado/controle/ClassMovimentacao.java
+++ CaixaEletronicoProjeto/src/br/edu/grupointegrado/controle/ClassMovimentacao.java
@@ -5,10 +5,50 @@
  */
 package br.edu.grupointegrado.controle;
 
+import br.edu.grupointegrado.conexao.ConexaoOracle;
+
 /**
  *
  * @author BSP
  */
-public class ClassMovimentacao {
+public class ClassMovimentacao extends ConexaoOracle {
+    private int cdMovimentacao;
+    private int cdOperacao;
+    private int dsMovimentacao;
+    private double vlMovimentacao;
+    private StringBuffer sql = new StringBuffer();
+
+    public int getCdMovimentacao() {
+        return cdMovimentacao;
+    }
+
+    public void setCdMovimentacao(int cdMovimentacao) {
+        this.cdMovimentacao = cdMovimentacao;
+    }
+
+    public int getCdOperacao() {
+        return cdOperacao;
+    }
+
+    public void setCdOperacao(int cdOperacao) {
+        this.cdOperacao = cdOperacao;
+    }
+
+    public int getDsMovimentacao() {
+        return dsMovimentacao;
+    }
+
+    public void setDsMovimentacao(int dsMovimentacao) {
+        this.dsMovimentacao = dsMovimentacao;
+    }
+
+    public double getVlMovimentacao() {
+        return vlMovimentacao;
+    }
+
+    public void setVlMovimentacao(double vlMovimentacao) {
+        this.vlMovimentacao = vlMovimentacao;
+    }
+    
     
 }
